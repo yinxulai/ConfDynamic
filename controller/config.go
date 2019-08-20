d236491133e4f6a072a1b882bc20aef7d41729a6
yinxulai <yinxulai@hotmail.com>
2019-08-20 14:14:12 +0800

    update

--- controller/config.go
+++ controller/config.go
@@ -1,9 +1,16 @@
 package controller
 
-import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+	"github.com/yinxulai/goutils/file"
+)
 
 // GetConfigs 获取配置信息
 func GetConfigs(ctx iris.Context) {
+	var data [1]module.Config
+
+	file.ReadJSON("./configs.json", &data)
+
 	ctx.ServeFile("./public/index.html", false)
 }
 
--- module/config.go
+++ module/config.go
@@ -2,20 +2,7 @@ package module
 
 // Config 配置
 type Config struct {
-	Name     string            // 名称
-	Identity string            // 唯一标识符
-	Fields   map[string]*Field // 字段
-}
-
-func (c *Config) OutReal() map[string]interface{} {
-	data := map[string]interface{}{}
-
-	if len(c.Fields) <= 0 {
-		return data
-	}
-
-	for _, field := range c.Fields {
-		return field.OutReal()
-	}
-	return data
+	Name    string
+	State   uint64
+	Context string
 }
