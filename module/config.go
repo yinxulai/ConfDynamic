ceb7fe7e123b980216867b340bd5c6ddf9a9354a
yinxulai <yinxulai@hotmail.com>
2019-08-20 20:46:28 +0800

    update

--- module/config.go
+++ module/config.go
@@ -2,7 +2,7 @@ package module
 
 // Config 配置
 type Config struct {
-	Name    string
-	Enable   bool
-	Context string
+	Name    string `json:"name"`
+	Enable  bool   `json:"enable"`
+	Context string `json:"context"`
 }
